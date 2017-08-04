140905dc3b9f22758afe819926c958e033f0f3ca
Demis Bellot <demis.bellot@gmail.com>
2017-08-04 02:45:49 -0400

    Move CustomTemplateFilters into its own class

---
+++ src/CustomTemplateFilters.cs
@@ -0,0 +1,44 @@
+
+using System.Linq;
+using System.Collections.Generic;
+using ServiceStack.Templates;
+
+namespace MvcTemplates
+{
+    public class CustomTemplateFilters : TemplateFilter
+    {
+        public Dictionary<int, KeyValuePair<string, string>> DocsIndex { get; } = new Dictionary<int, KeyValuePair<string, string>>();
+
+        public object prevDocLink(int order)
+        {
+            if (DocsIndex.TryGetValue(order - 1, out KeyValuePair<string,string> entry))
+                return entry;
+            return null;
+        }
+
+        public object nextDocLink(int order)
+        {
+            if (DocsIndex.TryGetValue(order + 1, out KeyValuePair<string,string> entry))
+                return entry;
+            return null;
+        }
+
+        List<KeyValuePair<string,string>> sortedDocLinks;
+        public object docLinks() => sortedDocLinks ?? (sortedDocLinks = sortLinks(DocsIndex));
+
+        public List<KeyValuePair<string,string>> sortLinks(Dictionary<int, KeyValuePair<string,string>> links)
+        {
+            var sortedKeys = links.Keys.ToList();
+            sortedKeys.Sort();
+
+            var to = new List<KeyValuePair<string,string>>();
+
+            foreach (var key in sortedKeys)
+            {
+                var entry = links[key];
+                to.Add(entry);
+            }
+
+            return to;
+        }
+    }}
--- src/Startup.cs
+++ src/Startup.cs
@@ -41,6 +41,8 @@ namespace MvcTemplates
             { "view-engine", "View Engine"},
             { "model-view-controller", "Model View Controller"},
             { "code-pages", "Code Pages"},
+            { "mvc-netcore", "ASP.NET Core MVC"},
+            { "mvc-aspnet", "ASP.NET v4.5 MVC"},
             { "sandbox", "Sandbox"},
             { "api-reference", "API Reference"},
         };
@@ -100,41 +102,4 @@ namespace MvcTemplates
         }
     }
 
-    public class CustomTemplateFilters : TemplateFilter
-    {
-        public Dictionary<int, KeyValuePair<string, string>> DocsIndex { get; } = new Dictionary<int, KeyValuePair<string, string>>();
-
-        public object prevDocLink(int order)
-        {
-            if (DocsIndex.TryGetValue(order - 1, out KeyValuePair<string,string> entry))
-                return entry;
-            return null;
-        }
-
-        public object nextDocLink(int order)
-        {
-            if (DocsIndex.TryGetValue(order + 1, out KeyValuePair<string,string> entry))
-                return entry;
-            return null;
-        }
-
-        List<KeyValuePair<string,string>> sortedDocLinks;
-        public object docLinks() => sortedDocLinks ?? (sortedDocLinks = sortLinks(DocsIndex));
-
-        public List<KeyValuePair<string,string>> sortLinks(Dictionary<int, KeyValuePair<string,string>> links)
-        {
-            var sortedKeys = links.Keys.ToList();
-            sortedKeys.Sort();
-
-            var to = new List<KeyValuePair<string,string>>();
-
-            foreach (var key in sortedKeys)
-            {
-                var entry = links[key];
-                to.Add(entry);
-            }
-
-            return to;
-        }
-    }
 }
